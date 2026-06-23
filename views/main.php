d6e8f5f4a6854843f91d4e1f0978a4669108e8bb
C. Maj <52975048+chrsmj@users.noreply.github.com>
2026-06-23 09:32:41 -0600

    another test string to forward an extension

--- views/main.php
+++ views/main.php
@@ -34,7 +34,7 @@
 			<div class="oc-sidebar-body">
 				<div class="oc-guide-group">
 					<button class="oc-quick-btn oc-write-btn" data-paste="create extension <ext> for <name>">create extension [ext] for [name]</button>
-					<button class="oc-quick-btn oc-write-btn" data-paste="forward <ext> to <number>">forward [ext] to [number]</button>
+					<button class="oc-quick-btn oc-write-btn" data-paste="forward <ext> to <number>"><?php echo _("forward [ext] to [number]"); ?></button>
 					<button class="oc-quick-btn oc-write-btn" data-paste="enable voicemail on <ext>"><?php echo _("enable voicemail on [ext]"); ?></button>
 					<button class="oc-quick-btn oc-write-btn" data-paste="block <number>">block [number]</button>
 					<button class="oc-quick-btn oc-write-btn" data-paste="add inbound route <DID> to <dest>">add inbound route [DID] to [dest]</button>
